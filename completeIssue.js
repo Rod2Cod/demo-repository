9039c62e78e9ed0909be14c68de76d1ec541f7f8
Maggion Alberto <74048267+Porror@users.noreply.github.com>
2024-10-28 19:00:32 +0100

    Update completeIssue.js

--- completeIssue.js
+++ completeIssue.js
@@ -1,24 +1,12 @@
+import { Octokit } from "@octokit/action";
 const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
 
-async function completedIssues() {
-  const { data: issues } = await octokit.request(`GET /repos/${process.env.GITHUB_REPOSITORY}/issues`, {
-    state: 'open'
-  });
+const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
 
-  for (const issue of issues) {
-    const body = issue.body || "";
-    const tasks = body.match(/- \[([ xX])\]/g) || [];
-
-    const allTasksCompleted = tasks.length > 0 && tasks.every(task => task.includes('[x]') || task.includes('[X]'));
-
-    if (allTasksCompleted) {
-      await octokit.request('PATCH /repos/${process.env.GITHUB_REPOSITORY}/issues/{issue_number}', {
-        issue_number: issue.number,
-        state: 'closed'
-      });
-      console.log(`Closed issue #${issue.number} - "${issue.title}"`);
-    }
-  }
-}
-
-closeCompletedIssues().catch(error => console.error(error));
+// See https://developer.github.com/v3/issues/#create-an-issue
+const { data } = await octokit.request("POST /repos/{owner}/{repo}/issues", {
+  owner,
+  repo,
+  title: "My test issue",
+});
+console.log("Issue created: %s", data.html_url);
